b29d408f37ef51a35d788a3b9ab1f0224347b575
HoangAn <an2419998@gmail.com>
2020-06-28 14:14:47 +0700

    finish lesson 1

---
+++ rn-first-app/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar"
+import React, { useState } from "react"
+import { StyleSheet, Text, View, Button } from "react-native"
+
+export default function App() {
+  const [outputText, setOutputText] = useState("Hello World")
+  return (
+    <View style={styles.container}>
+      <Text>{outputText}</Text>
+      <Button
+        title="Change Text"
+        onPress={() => setOutputText("My name is An")}
+      />
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+})
